898dd2253d5fa542f3b697fc617dc597e9726b5f
Joshua Kapilian <kapgamesandapps2@gmail.com>
2022-04-05 23:54:46 -0400

    Create new website

---
+++ main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+	let box = $(".box")
+	box.hide();
+	box.fadeIn({"queue": false, "duration": 1000});
+	box.animate({"margin-top": "50px"}, 1000);
+
+	$('.image-carousel').slick({
+		centerMode: true,
+		centerPadding: '80px',
+		slidesToShow: 3,
+		focusOnSelect: true,
+		responsive: [
+			{
+			breakpoint: 768,
+			settings: {
+				arrows: true,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 3,
+				dots: true,
+			}
+			},
+			{
+			breakpoint: 480,
+			settings: {
+				arrows: true,
+				centerMode: true,
+				centerPadding: '40px',
+				slidesToShow: 1,
+				dots: true,
+			}
+			}
+		]
+	});
+	
+	
+	$("#about-link").click(function() {
+		$([document.documentElement, document.body]).animate({
+			scrollTop: $("#about").offset().top
+		}, 1000);
+		window.location.href = "/#about";
+	});
+
+	$("#gameplay-link").click(function() {
+		$([document.documentElement, document.body]).animate({
+			scrollTop: $("#gameplay").offset().top
+		}, 1000);
+		window.location.href = "/#gameplay";
+	});
+		
+
+
+});
